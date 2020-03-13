aa4c4360f9b3c7756611cb862cc0d9addce243a6
joelpdurham <joeldur@gmail.com>
2020-03-13 12:26:06 -0700

    finished all inputs for auth form

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -2,12 +2,31 @@ import React, { useState } from 'react';
 
 export const SignUp = () => {
   const [userName, setUserName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   return (
     <section>
       <form>
-        <label>Username: <input type="text" value={userName} onChange={({ target }) => setUserName(target.value)} /></label>
+        <label>Username: 
+          <input type="text" 
+            value={userName} 
+            onChange={({ target }) => setUserName(target.value)} />
+        </label>
+
+        <label>Email: 
+          <input type="text" 
+            value={email} 
+            onChange={({ target }) => setEmail(target.value)} />
+        </label>
+
+        <label>Username: 
+          <input type="password" 
+            value={password} 
+            onChange={({ target }) => setPassword(target.value)} />
+        </label>
       </form>
+
     </section>
   );
 }
