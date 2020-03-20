2b5f64e16a1e308c9defaec4d1a8e5e9229e1e5a
joelpdurham <joeldur@gmail.com>
2020-03-20 15:01:53 -0700

    unread count tracks

--- src/components/profile/Positives.js
+++ src/components/profile/Positives.js
@@ -5,6 +5,7 @@ import { Positive } from './Positive';
 
 export const Positives = () => {
   const [currentPositive, setCurrentPositive] = useState();
+  const [totalUnread, setTotalUnread] = useState(0);
 
   useEffect(() => {
     getNewCurrentPositive();
@@ -14,7 +15,11 @@ export const Positives = () => {
     getMessages()
       .then(positives => {
         const unread = positives.find(positive => !positive.seen);
-        console.log(unread);
+        const countUnread = positives.reduce((acc, positive) => {
+          if(!positive.seen) acc = acc + 1;
+          return acc;
+        }, 0);
+        setTotalUnread(countUnread);
         if(unread) {
           setCurrentPositive(unread);
           updateMessage(unread._id, { seen: true });
@@ -34,6 +39,7 @@ export const Positives = () => {
   return (
     <section>
       <h1>Positives</h1>
+      <h2>Unread: {totalUnread > 0 ? totalUnread - 1 : totalUnread}</h2>
       {render}
       <button onClick={getNewCurrentPositive}>Get Another</button>
     </section>
